bfbf605e50a83512a682b2c9c0c8a168bbc91831
Jessica Flores <jafloresrgv@gmail.com>
2018-07-31 15:29:16 -0500

    started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashComponent } from './dash/dash.component';
+import { QaComponent } from './qa/qa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashComponent,
+    QaComponent
   ],
   imports: [
     BrowserModule
